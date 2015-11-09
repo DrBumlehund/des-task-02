71d151c8fc1a06292d92336baaa9346b70995f9c
Thomas August Lemqvist <dr.bumlehund@live.dk>
2015-11-09 16:07:32 +0100

    game of life

--- des-task-2/Game of Life/Program.cs
+++ des-task-2/Game of Life/Program.cs
@@ -8,36 +8,88 @@ namespace Game_of_Life
 {
     class Program
     {
+        private int rows, cols, sleepTimer; 
+        int[,] cell;
         static void Main()
         {
-            var rows = 10; // Including a 1-cell border o'death.
-            var cols = 10;
-            var rnd = new Random();
-            var cell = new int[rows, cols];
-            Do(cell, rows, cols, (r, c) => { cell[r, c] = (int)Math.Round(rnd.NextDouble()); });
-            while (true)
-            {
-                WriteCells(cell, rows, cols);
-                Console.WriteLine("--------");
-                Console.ReadLine();
-                cell = Gen(cell, rows, cols);
-            }
+
+            Program gol = new Program();
+
+            gol.Startup();
+
+            
+            
         }
+
+       
 
         static IEnumerable<int> Rg = Enumerable.Range(-1, 3);
 
-        static int Nbrs(int[,] cell, int r, int c)
+        private void Startup()
+        {
+            try
+            {
+                Console.Write("Enter size:  ");
+                rows = Convert.ToInt32(Console.ReadLine());
+                cols = rows;
+                Console.Write("Enter delay [ms]:  ");
+                sleepTimer = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter numbers in integers");
+            }
+
+            cell = new int[rows, cols];
+            var rnd = new Random();
+            Do(cell, rows, cols, (r, c) => { cell[r, c] = (int)Math.Round(rnd.NextDouble()); });
+
+            setTicks();
+
+
+        }
+
+        private void setTicks()
+        {
+            Console.Write("Enter ticks:  ");
+            Play(Convert.ToInt32(Console.ReadLine()));
+        }
+
+        private void Play(int ticks)
+        {
+            for (int i = 0; i < ticks; i++)
+            {
+                WriteCells(cell, rows, cols);
+                System.Threading.Thread.Sleep(sleepTimer);
+                
+                cell = Gen(cell, rows, cols);
+            }
+            Console.WriteLine("Continue? (Y/N)");
+            if (Console.ReadLine().ToLower().Equals("y")){
+                setTicks();
+
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Thank you for playing");
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+
+        public int Nbrs(int[,] cell, int r, int c)
         {
             return Rg.Sum(dr => Rg.Sum(dc => cell[r + dr, c + dc]));
         }
 
-        static int Next(int[,] cell, int r, int c)
+        public int Next(int[,] cell, int r, int c)
         {
             var nbrs = Nbrs(cell, r, c) - cell[r, c];
             return (cell[r, c] == 0 && nbrs == 3 || cell[r, c] == 1 && (nbrs == 2 || nbrs == 3)) ? 1 : 0;
         }
 
-        static void Do(int[,] cell, int rows, int cols, Action<int, int> a)
+        public void Do(int[,] cell, int rows, int cols, Action<int, int> a)
         {
             for (var r = 1; r < rows - 1; r++)
             {
@@ -48,17 +100,19 @@ namespace Game_of_Life
             }
         }
 
-        static int[,] Gen(int[,] cell, int rows, int cols)
+        public int[,] Gen(int[,] cell, int rows, int cols)
         {
             var nxt = new int[rows, cols];
             Do(cell, rows, cols, (r, c) => { nxt[r, c] = Next(cell, r, c); });
             return nxt;
         }
 
-        static void WriteCells(int[,] cell, int rows, int cols)
+        public void WriteCells(int[,] cell, int rows, int cols)
         {
-            Do(cell, rows, cols, (r, c) => {
-                Console.Write(cell[r, c] == 0 ? ' ' : '*');
+            Console.Clear();
+            Do(cell, rows, cols, (r, c) =>
+            {
+                Console.Write(cell[r, c] == 0 ? '-' : '*');
                 if (c == cols - 2) Console.WriteLine();
             });
         }
